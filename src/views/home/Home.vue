98861b12583019dd852bc3e7c6de5e091678ddb1
Annietwq <540969329@qq.com>
2021-08-08 10:53:59 +0800

    123

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,44 +1,169 @@
 <template>
-<div id="home">
-	<nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-	<home-swiper :banners="banners"/>
-	<home-recommend-view :recommends="recommends"/>
-</div>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <home-swiper :banners="banners" />
+    <home-recommend-view :recommends="recommends" />
+    <home-feature-view />
+    <tab-control class="tab-control" :titles="['流行', '新款', '精选']" />
+    <ul>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+      <li>12</li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import NavBar from 'components/common/navbar/NavBar'
-import HomeSwiper from './childComps/HomeSwiper.vue'
-import HomeRecommendView from './childComps/HomeRecommendView.vue'
-import {getHomeMultidata} from 'network/home'
+import HomeSwiper from "./childComps/HomeSwiper.vue";
+import HomeRecommendView from "./childComps/HomeRecommendView.vue";
+import HomeFeatureView from "./childComps/HomeFeatureView.vue";
+
+import NavBar from "components/common/navbar/NavBar";
+import TabControl from "components/content/tabControl/TabControl.vue";
+
+import { getHomeMultidata } from "network/home";
 
 export default {
-	name:"Home",
-	components:{
-		NavBar,
-		HomeSwiper,
-		HomeRecommendView,
-	},
-	data(){
-		return {
-			banners:[], //获取请求数据中的banner中的list
-			recommends:[], //获取请求数据中的recommend中的list
-		}
-	},
-	created(){
-		// 1.请求多个数据
-		getHomeMultidata().then(res => {
-			console.log(res);
-			this.banners = res.data.banner.list;
-			this.recommends = res.data.recommend.list;
-		})
-	}
-}
+  name: "Home",
+  components: {
+    HomeSwiper,
+    HomeRecommendView,
+    HomeFeatureView,
+    NavBar,
+    TabControl,
+  },
+  data() {
+    return {
+      banners: [], //获取请求数据中的banner中的list
+      recommends: [], //获取请求数据中的recommend中的list
+    };
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then((res) => {
+      console.log(res);
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
+  },
+};
 </script>
 
 <style scoped>
+#home {
+  padding-top: 44px;
+}
+
 .home-nav {
-	background-color: var(--color-tint);
-	color: #fff;
+  background-color: var(--color-tint);
+  color: #fff;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  z-index: 9999;
+}
+
+.tab-control {
+	/* 没有达到top:44px 的时候-> position:sticky ,达到之后-> position:fixed*/
+  position: sticky;
+  top: 44px;
 }
 </style>
